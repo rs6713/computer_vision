c022c9274fc76ae77f2acb78acef8093de68626e
Becks Simpson <rebecca.simpson13@imperial.ac.uk>
2017-03-13 23:29:14 +0000

    Add files via upload

--- RF_2017/RF_2017/internal/splitNode.m
+++ RF_2017/RF_2017/internal/splitNode.m
@@ -2,7 +2,7 @@ function [node,nodeL,nodeR] = splitNode(data,node,param)
 % Split node
 
 visualise = 0;
-splitfunct="randrange";%other split functions
+%splitfunct="randrange";%other split functions
 
 % Initilise child nodes
 iter = param.splitNum;
@@ -21,28 +21,35 @@ data = data(idx,:);
 ig_best = -inf; % Initialise best information gain
 idx_best = [];
 for n = 1:iter
-    
+    t=0;
+    dim=-1;
+    idx_=[];
     % Split function - Modify here and try other types of split function
-    if splitfunct=="randrange"
+    if strcmp(param.split,'IG')
         dim = randi(D-1); % Pick one random dimension
         d_min = single(min(data(:,dim))) + eps; % Find the data range of this dimension
         d_max = single(max(data(:,dim))) - eps;
         t = d_min + rand*((d_max-d_min)); % Pick a random value within the range as threshold
         idx_ = data(:,dim) < t;
+    else
+        if strcmp(param.split,'twopixel')
+            dim=12;
+            d1_min = single(min(data(:,1))) - eps; % Find the data range of this dimension
+            d1_max = single(max(data(:,1))) + eps;
+            d2_min = single(min(data(:,2))) + eps; % Find the data range of this dimension
+            d2_max = single(max(data(:,2))) - eps;
+            t = (d1_min + rand*((d1_max-d1_min))) - (d2_min + rand*((d2_max-d2_min))); % Pick a random value within the range as threshold
+            idx_ = (data(:,1)-data(:,2)) < t;
+        end
     end
-    if splitfunct=="twopixel"
-        d1_min = single(min(data(:,1))) + eps; % Find the data range of this dimension
-        d1_max = single(max(data(:,1))) - eps;
-        d2_min = single(min(data(:,2))) + eps; % Find the data range of this dimension
-        d2_max = single(max(data(:,2))) - eps;
-        t = d1_min + rand*((d1_max-d1_min)) - (d2_min + rand*((d2_max-d2_min))); % Pick a random value within the range as threshold
-        idx_ = (data(:,1)-data(:,2)) < t;
-    end
-    if splitfunct=="kmeans"
+    %{
+    if strcmp(param.split,'kmean')
+        dim=4;
         idx = kmeans(data,2);%cluster indes for each pt
         idx_= find(idx==1); % data(find(idx==1),:);
     end
-
+    %}
+    %idx_
     ig = getIG(data,idx_); % Calculate information gain
     
     if visualise
@@ -53,7 +60,12 @@ for n = 1:iter
     [node, ig_best, idx_best] = updateIG(node,ig_best,ig,t,idx_,dim,idx_best);
     
 end
-
+%tree(T).leaf(leaf_idx).label; to
+ %if ~isempty(tree(T).node(n).idx)
+  %   if ~tree(T).node(n).dim % if this is a leaf node dim=0
+   %      tree(T).leaf(cnt).label = cnt_total;
+   
+   %idx is logical array 0's, 1's
 nodeL.idx = idx(idx_best);
 nodeR.idx = idx(~idx_best);
 
--- RF_2017/RF_2017/internal/testTrees.m
+++ RF_2017/RF_2017/internal/testTrees.m
@@ -10,17 +10,31 @@ for T = 1:length(tree)%number of trees
             t = tree(T).node(idx).t;
             dim = tree(T).node(idx).dim;
             % Decision
-            if data(m,dim) < t % Pass data to left node
-                idx = idx*2;
+            %if axis alligned
+            if dim==12
+                if (data(m,1)-data(m,2)) < t % Pass data to left node
+                    idx = idx*2;
+                else
+                    idx = idx*2+1; % and to right
+                end
+            %if two pixel test
             else
-                idx = idx*2+1; % and to right
+                 if data(m,dim) < t % Pass data to left node
+                    idx = idx*2;
+                else
+                    idx = idx*2+1; % and to right
+                end               
             end
             
         end
+        %idx is leaf node
         leaf_idx = tree(T).node(idx).leaf_idx;
         
         if ~isempty(tree(T).leaf(leaf_idx))
             p(m,:,T) = tree(T).leaf(leaf_idx).prob;%distribution at leaf
+            if(tree(T).leaf(leaf_idx).label==0)
+                printfn ('leaf_idx in tree(T).leaf that gives 0 label: %A',leaf_idx);
+            end
             label(m,T) = tree(T).leaf(leaf_idx).label;
             
 %             if isfield(tree(T).leaf(leaf_idx),'cc') % for clustering forest
